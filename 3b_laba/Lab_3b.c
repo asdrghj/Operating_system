059c0c760cf524a85b32f04762eef4ba7685d585
Меретукова  Бэлла <bella_m@router.lab209.kaf>
2019-12-19 21:54:07 +0300

    laba 3b

--- 3b_laba/Lab_3b.c
+++ 3b_laba/Lab_3b.c
@@ -21,17 +21,11 @@ main()
 	    int fifo_read = open("FIFO", O_RDONLY);
 	    if (fifo_read == -1)
 	    	printf("Fifo cannot open to read \n");
-	    char *buf = (char*) calloc (200, sizeof(char));
+	    char *buf = (char*) calloc (25, sizeof(char));
 	    read(fifo_read, buf, 5);
-	    
-	    //char *buf1 = (char*) calloc (10, sizeof(char));
-	    //buf1 = strncpy(buf1, buf, 5);
-	    //buf = strnset(buf, ' ', 5);
-	    //printf("CHILD: time in parent -- %s\n", buf); 
 	    printf("CHILD: parent pid  -- %s\n", buf);
-	    read(fifo_read, buf, 20);
+	    read(fifo_read, buf, 25);
 	    printf("CHILD: time in parent -- %s\n", buf); 
-	    //printf("CHILD parent pid -- %20s\n", buf[24]);
 	    close(fifo_read);
 	    sleep(2);
 	    time_t child_time = time(0);
@@ -41,29 +35,18 @@ main()
 	}
 
 	default:
-	{
-	    
+	{ 
 	    int fifo_write = open("FIFO", O_WRONLY);
 	    if(fifo_write == -1)
 		printf("Fifo cannot open to write \n");
 	    time_t parent_time = time(0);
 	    pid_t  parent_pid = getpid();
-	    //printf("%d\n", getpid());
-	    char *parent_pid_char;
-	    char buff[20];
+	    char buff[25];
 	    sprintf(buff, "%d", parent_pid);
-	    char *time_plus_pid = strcat(buff, ctime(&parent_time));
-	    //parent_pid_char = (char*)malloc(10*sizeof(char));
-	    //parent_pid_char = itoa(parent_pid, buff, 20);
-	    //write(fifo_write, ctime(&parent_time), sizeof(ctime(&parent_time)));
 	    printf("PARENT: My pid -- %d\n", parent_pid);
-
-	    //write(fifo_write, &parent_pid, strlen(pa));
+		printf("PARENT: My time -- %s\n", ctime(&parent_time));
 	    write(fifo_write, &buff, strlen(buff));
-	    write(fifo_write, ctime(&parent_time), 200);
-
-	    //write(fifo_write, time_plus_pid, strlen(time_plus_pid ));
-
+	    write(fifo_write, ctime(&parent_time), 50);
 	    close(fifo_write);
 	    break;
 	    
